bc5ce9795e08d029b23798426d3a3bdc5fe105b0
trongpham <phamvantrong7.qn@gmail.com>
2024-05-09 21:34:38 +0700

    Add login page (todo)

---
+++ IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel vm)
+        {
+            return View();
+        }
+    }
+}
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,33 @@
+using IdentityModel;
 using IdentityServer;
+using IdentityServer.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(config =>
+{
+    config.UseInMemoryDatabase("Memory");
+});
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "IdentityServer.Cookie";
+    config.LoginPath = "/Auth/Login";
+});
+
 
 //Identity server is just a toolbox allows us to set up the infrastructure for authen vs author using OIDC flows 
 builder.Services.AddIdentityServer()
@@ -12,6 +39,14 @@ builder.Services.AddIdentityServer()
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Seeding user
+using(var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var user = new IdentityUser("bob");
+    userManager.CreateAsync(user, "password").GetAwaiter().GetResult();
+}
 
 app.UseRouting();
 
